ad7291686cf30f2c4fdd6490ff121cca375b4861
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-12-15 11:04:05 +0100

    Update grid layout and add input field and button

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   const [tasks, setTasks] = useState(current.tasks);
 
   return (
-    <div className="grid grid-cols-5 grid-rows-5 gap-4 h-[100svh] w-full">
-      <div className="row-span-5">
+    <div className="grid grid-cols-7 grid-rows-[repeat(7,minmax(0,1fr))] gap-4 h-[100svh] w-full p-3">
+      <div className="[grid-row:span_7/span_7] border-4 border-black">
         <h1>History</h1>
         <div>
           {history.map(h => (
@@ -21,7 +21,7 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="col-span-4 row-span-4">
+      <div className="col-span-5 row-span-6 border-4 border-black">
         <h1>{current?.name}</h1>
         <div>
           {current?.tasks.map((task: any) => {
@@ -30,9 +30,18 @@ export default function Home() {
           })}
         </div>
       </div>
-      <div className="col-span-4 col-start-2 row-start-5">
-        <input type="text" />
-        <button>submit</button>
+      <div className="col-span-5 col-start-2 row-start-7 grid grid-cols-[1fr,12rem] gap-4">
+        <input type="text" className="border-4 border-black h-full w-full" />
+        <button className="border-4 border-black h-full w-full">submit</button>
+      </div>
+
+      <div className="[grid-row:span_7/span_7] border-4 border-black">
+        <h1>History</h1>
+        <div>
+          {history.map(h => (
+            <div key={h.id}>{h.name}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
