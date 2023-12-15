fcaafaf4e62aa0fc4997007985e83d46aa0aaa79
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-12-15 11:58:55 +0100

    Refactor page component layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,18 @@ export default function Home() {
   const [project, setProject] = useState(projects[0]);
   const [history, setHistory] = useState(project.history);
   const [current, setCurrent] = useState(project.history[0]);
-  const [tasks, setTasks] = useState(current.tasks);
 
   return (
     <div className="grid grid-cols-[1fr,6fr] gap-4 h-[100svh] w-full p-3">
-      <div className="border border-black rounded-2xl">
-        <h1>History</h1>
-        <div>
+      <div className="border border-black rounded-2xl p-4">
+        <h1 className="text-3xl font-bold">History</h1>
+        <div className="pt-8">
           {history.map(h => (
-            <div key={h.id}>{h.name}</div>
+            <button key={h.id} className="border w-full px-2 p-1 rounded-lg">{h.name}</button>
           ))}
         </div>
       </div>
-      <div className="border border-black rounded-2xl grid grid-rows-[auto,1fr,auto] overflow-hidden">
+      <div className="border border-black rounded-2xl grid grid-rows-[auto,1fr,auto] overflow-hidden p-4">
         <h1>{current?.name}</h1>
         <div>
           {current?.tasks.map((task: any) => {
@@ -29,7 +28,7 @@ export default function Home() {
             return <div key={task.id}>{details.task}</div>;
           })}
         </div>
-        <div className="w-full flex p-4 gap-4">
+        <div className="w-full flex gap-4">
           <input type="text" className="w-full border p-4 rounded-2xl" />
           <button className="p-4 border rounded-2xl px-8">s</button>
         </div>
